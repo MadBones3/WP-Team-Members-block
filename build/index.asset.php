9f0811d6db91e8751cdd6fe8b0340ffa1e2cab4c
Ashton Blignaut <ashblignaut@gmail.com>
2022-07-20 17:02:02 +0200

    team members plugin changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '4ca9a15aa1722fca9314');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'a9e565e9dffd90247f84');
